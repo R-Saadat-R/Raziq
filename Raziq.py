ce3b292e8e567fb815249ca026b79922da2f0202
R-Saadat-R <131810876+R-Saadat-R@users.noreply.github.com>
2023-05-07 09:44:04 +0430

    Update Raziq.py

--- Raziq.py
+++ Raziq.py
@@ -469,6 +469,9 @@ class Main:
 				self.id.append(__+str(_))
 			
 			ps_limit = int(input(' How many passwords do you want to add ? '))
+			except:
+			ps_limit =1
+		linex()
 			with ThreadPoolExecutor(max_workers=30) as coeg:
 				print("\n\033[1;32m [!] Ex(123456,100200,500600,786786,etc..) FOR Old IDZ\033[1;37m ")
 				listpass = input("%s [?] ENTER PASSWORD :%s "%(G,Y))
@@ -899,6 +902,9 @@ class Main:
 				self.id.append(__+str(_))
 			
 			ps_limit = int(input(' How many passwords do you want to add ? '))
+			except:
+			ps_limit =1
+		linex()
 			with ThreadPoolExecutor(max_workers=30) as coeg:
 				print("\n\033[1;32m [!] Ex(123456,100200,500600,786786,etc..) FOR Old IDZ\033[1;37m ")
 				listpass = input("%s [?] ENTER PASSWORD :%s "%(G,Y))
